db3622e928e1b867e6d5dd379776965e3ff1eb25
wright <wrightgichana@gmail.com>
2026-01-29 11:08:57 +0300

    backend fixes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -53,6 +53,7 @@ let login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure:true,
+            sameSite: "none",
             path:"/",
             maxAge: 24 * 60 * 60 * 1000
         });
